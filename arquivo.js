01f702cea2c1557ecba65d450aff2ffce2fa2654
alexssandrolira <alexlira12312@gmail.com>
2021-09-11 16:28:55 -0300

    mudanças

---
+++ arquivo.js
@@ -0,0 +1,17 @@
+const numero1 = 15
+const numero2 = 35
+const verdadeiro = true
+
+//ternario
+//alert(numero1 > numero2 ? 'Numero 1 > 2' : 'Numero 1 Nao > 2')
+
+/*
+const variavel = ['Levi', 'alex', 'toin', 332]
+const resul = variavel.join(':')
+alert(resul)
+*/
+
+const pessoas = [ 'alex', ' erick' , 'hipolito']
+for(let i = 0; pessoas.length; i++){
+    console.log(pessoas[i])
+}
