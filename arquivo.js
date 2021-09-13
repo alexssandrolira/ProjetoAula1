d3718c83d4a9ee421dc5b355f4985f2ab2c5593a
alexssandrolira <alexlira12312@gmail.com>
2021-09-12 21:39:00 -0300

    mais anotações

--- arquivo.js
+++ arquivo.js
@@ -11,7 +11,67 @@ const resul = variavel.join(':')
 alert(resul)
 */
 
-const pessoas = [ 'alex', ' erick' , 'hipolito']
-for(let i = 0; pessoas.length; i++){
-    console.log(pessoas[i])
+/*
+function fabricaPessoa(nomeDaPessoa,sobrenomeDaPessoa,dataNascimento){
+    return{
+        nome: nomeDaPessoa,
+        sobrenome: sobrenomeDaPessoa,
+        datanascimento: dataNascimento
+    }
 }
+
+const pessoa1 = fabricaPessoa('alex', 'lira', 35)
+const pessoa2 = fabricaPessoa('pop', 'rei', 23)
+const pessoa3 = fabricaPessoa('sandro', 'gomes', 12)
+const pessoa4 = fabricaPessoa('lucas', 'santos', 54)
+const pessoa5 = fabricaPessoa('val', 'rocha', 34)
+const pessoas= [pessoa1,pessoa2,pessoa3,pessoa4,pessoa5]
+
+function lerPessoa(pessoa) {
+    console.log("Dentro do for lemos", pessoa)
+}
+pessoas.forEach(lerPessoa)
+
+/*for(const pessoa of pessoas){
+    console.log("Dentro do for lemos", pessoa)
+}
+*/
+
+function funcaocustosa() {
+    return new Promise(function (resolve,reject){
+        //vai rodar de forma assíncrona
+        const resultado = 10 + 5
+        resolve(resultado)
+    })
+}
+
+function esperar(segundos){
+    return new Promise(function(resolve,reject){
+        setTimeout(resolve, segundos * 1000)
+    })
+}
+
+async function principal() {
+    await esperar(5)
+    console.log('passou-se 5 segundos')
+}
+
+principal()
+
+funcaocustosa().then(function (resultado){
+console.log(resultado)
+})
+
+
+const calculo = () => {
+    return new Promise(function(resolve,reject){
+        const calcular = 40+3
+        resolve(calcular)
+    })
+}
+
+calculo().then(function (calcular){
+    console.log(calcular)
+})
+
+
